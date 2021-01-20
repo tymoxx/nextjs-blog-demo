3dfcd581200a1f2a7a58221325e4a3cf75520b5a
tymoxx <7777kara@gmail.com>
2021-01-20 17:34:48 +0100

    adds styling

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,12 @@
 import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import {getAllPostIds, getPostData} from '../../lib/posts'
+import Head from 'next/head'
+import utilStyles from '../../styles/utils.module.css'
+import Date from "../../components/date";
 
-export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+
+export async function getStaticProps({params}) {
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
@@ -13,11 +17,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
